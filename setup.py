b0c140c0d76c8ebb1e8cd7d8992f8fdd1a83c03f
Matthew D. Scholefield <matthew331199@gmail.com>
2019-07-04 16:41:48 -0700

    Increment version to 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='flask-resto',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/matthewscholefield/flask-resto',
     license='MIT',
     author='Matthew Scholefield',
